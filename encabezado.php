a2797265a827d87a05fc3e62c03711c40c6c561b
greenyetti <80862816+greenyetti@users.noreply.github.com>
2021-06-09 17:54:23 -0700

    Add files via upload

---
+++ encabezado.php
@@ -0,0 +1,36 @@
+<?php
+/*
+	Pequeño, muy pequeño sistema de ventas en PHP con MySQL
+
+	@author parzibyte
+
+	No olvides visitar parzibyte.me/blog para más cosas como esta
+*/
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Vinit Ventas</title>
+	
+	<link rel="stylesheet" href="./css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="./css/2.css">
+	<link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-danger navbar-fixed-top"  class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#"><img alt="https://vvb.com.mx/wp/wp-content/uploads/2020/12/Artboard-3.png" src="https://vvb.com.mx/wp/wp-content/uploads/2020/12/Artboard-3.png" style="max-height: 30px" /></a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a  class="text-light" href="./listar.php">Productos</a></li>
+					<li><a  class="text-light" href="./vender.php">Vender</a></li>
+					<li><a  class="text-light" href="./ventas.php">Ventas</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
---
+++ ventas.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-06-2021 a las 02:49:23
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ventas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `codigo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `precioVenta` decimal(5,2) NOT NULL,
+  `precioCompra` decimal(5,2) NOT NULL,
+  `existencia` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `codigo`, `descripcion`, `precioVenta`, `precioCompra`, `existencia`) VALUES
+(6, '1', 'Lapostolle Merlot Cuvee Alexander 750ml', '390.00', '390.00', '10.00'),
+(7, '2', 'Senorio De Sarria Rose 750m', '380.00', '380.00', '19.00'),
+(8, '3', 'Monte Xanic Grenache 750ml', '280.00', '280.00', '15.00'),
+(9, '4', 'Adobe Guadalupe Uriel 750ml', '499.00', '499.00', '19.00'),
+(10, '7', 'Santo Tomas Duet 750ml', '580.00', '580.00', '10.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos_vendidos`
+--
+
+CREATE TABLE `productos_vendidos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_producto` bigint(20) UNSIGNED NOT NULL,
+  `cantidad` bigint(20) UNSIGNED NOT NULL,
+  `id_venta` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `fecha` datetime NOT NULL,
+  `total` decimal(7,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id`, `fecha`, `total`) VALUES
+(1, '2021-05-29 03:27:57', '0.00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos_vendidos`
+--
+ALTER TABLE `productos_vendidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_venta` (`id_venta`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `productos_vendidos`
+--
+ALTER TABLE `productos_vendidos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos_vendidos`
+--
+ALTER TABLE `productos_vendidos`
+  ADD CONSTRAINT `productos_vendidos_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `productos_vendidos_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
